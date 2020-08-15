249e2a589cae3beb8b1948da276bdde862abeae8
Sam Wight <samuelwight@gmail.com>
2020-08-15 09:03:07 -0500

    Add Google Analytics.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import React from "react"
+import { Helmet } from "react-helmet"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -6,6 +7,24 @@ import DateCalculator from "../components/DateCalculator"
 
 const IndexPage = () => (
   <Layout>
+    <Helmet>
+      <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=UA-134173534-3"
+      ></script>
+      <script>
+        {`window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'UA-134173534-3');`}
+      </script>
+      <script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+        data-ad-client="ca-pub-8789074411525323"
+      />
+    </Helmet>
     <SEO title="When did my parents fuck?" />
     <h1 className="text-5xl text-center">When did my parents fuck?</h1>
     <h2 className="text-lg text-center">
